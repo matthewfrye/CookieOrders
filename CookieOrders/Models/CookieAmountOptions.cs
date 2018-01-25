252ea98c1857b3761715111b44f785dfe3cade9d
matthewfrye <fryems@gmail.com>
2018-01-24 18:23:51 -0600

    attempt at a Girl Scout Cookie site

---
+++ CookieOrders/Models/CookieAmountOptions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CookieOrders.Models
+{
+    public static class HtmlLists
+    {
+        public static IEnumerable<CookieAmount> CookieAmounts = new List<CookieAmount> {
+    new CookieAmount {
+        Value = 0,
+        Text = "0"
+    },
+    new CookieAmount {
+        Value = 1,
+        Text = "1"
+    },
+    new CookieAmount {
+        Value = 2,
+        Text = "2"
+    },
+    new CookieAmount {
+        Value = 3,
+        Text = "3"
+    },
+    new CookieAmount {
+        Value = 4,
+        Text = "4"
+    },
+    new CookieAmount {
+        Value = 5,
+        Text = "5"
+    },
+    new CookieAmount {
+        Value = 6,
+        Text = "6"
+    },
+    new CookieAmount {
+        Value = 7,
+        Text = "7"
+    },
+    new CookieAmount {
+        Value = 8,
+        Text = "8"
+    },
+    new CookieAmount {
+        Value = 9,
+        Text = "9"
+    }
+};
+    }
+
+    public class CookieAmount
+    {
+        public int Value { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ CookieOrders/Models/CookieOrderViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CookieOrders.Models
+{
+    public class CookieOrderViewModel
+    {
+        public List<Cookie> Cookies { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string AddressLine1 { get; set; }
+
+        public int TotalDue { get {
+                int total = 0;
+                foreach(var cookie in Cookies)
+                {
+                    if (cookie.Type == CookieType.GlutenFreeTrios)
+                        total += cookie.Amount * 5;
+                    else
+                        total += cookie.Amount * 4;
+                }
+                return total;
+            } }
+
+
+        public CookieOrderViewModel()
+        {
+
+            Cookies = new List<Cookie>();
+            foreach(CookieType type in Enum.GetValues(typeof(CookieType)))
+            {
+                Cookies.Add(new Cookie { Type = type });
+
+            }
+        }
+    }
+}
