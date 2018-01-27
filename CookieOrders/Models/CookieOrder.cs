71f860e19dda24ba71eae517b87c597e1906356e
matthewfrye <fryems@gmail.com>
2018-01-27 10:10:22 -0600

    small updates

---
+++ CookieOrders/Models/CookieOrder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CookieOrders.Models
+{
+    public class CookieOrder
+    {
+        public IList<Cookie> Cookies { get; set; }
+        public string Name { get; set; }
+        public string AddressLine1 { get; set; }
+        public string City { get; set; }
+        public string Email { get; set; }
+
+    }
+}
